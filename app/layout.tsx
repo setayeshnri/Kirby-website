111a378bf168b4aba291ae81b5f8551fdd659e69
setayeshnri <setayeshn1382@gmail.com>
2024-09-07 12:42:35 +0200

    changed file structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 "use client"
 import "@/styles/globals.css";
 import Home from "./page";
+import App from "@/components/navbar";
 
 export default function RootLayout() {
   return (
     <html suppressHydrationWarning lang="en">
       <head />
       <body>
-        <Home/>
-     
+        <App />
+        <Home />
       </body>
     </html>
   );
--- components/button.tsx
+++ components/button.tsx
@@ -1,15 +1,17 @@
-import { forwardRef } from "react";
+import { forwardRef, useRef} from "react";
 import { Button } from "@nextui-org/button";
 import { Rounded } from "@/config/fonts";
-const MyButton = forwardRef(() => {
+interface buttonProps {
+  btnRef: React.RefObject<HTMLButtonElement>
+}
+const MyButton = forwardRef(({btnRef}: buttonProps,ref) => {
+
   return (
-    <Button
-      className="bg-primary-blue w-[16%] h-16 z-100 rounded-[2.4rem] text-white text-2xl font-extrabold tracking-wide shadow-[0px_2px_16px_0px_#00000030,inset_0px_-2px_4px_0px_#0000001a,inset_0px_4px_6px_0px_#ffffff42]
+    <Button ref={btnRef}
+      className="button primaryBtn bg-primary-blue w-[16%] h-16 z-100 rounded-[2.4rem] text-white text-2xl font-extrabold tracking-wide shadow-[0px_2px_16px_0px_#00000030,inset_0px_-2px_4px_0px_#0000001a,inset_0px_4px_6px_0px_#ffffff42]
   "
       style={{ fontFamily: '"M PLUS Rounded 1c", sans-serif' }}
-    >
-      Learn more
-    </Button>
+    ></Button>
   );
 });
 
