8bd8443b8e2fa1222a14a9ecd340df77785be84f
Mehrshad Baqerzadegan <MehrshadBaqerzadegan@gmail.com>
2024-09-27 14:06:03 +0330

    Fix: self center

--- app/pages/chatBot/page.tsx
+++ app/pages/chatBot/page.tsx
@@ -16,11 +16,11 @@ const Home: React.FC = () => {
   return (
     <div className=" bg-[url('/chatBot/gptbg.png')] max-lg:bg-[url('/chatBot/wallpaper.png')] flex flex-row justify-around h-[100vh] bg-cover overflow-hidden ">
       <KirbyAnimation />
-      <div className="relative top-[5vw] w-[36vw] h-[40vw] max-lg:w-[70%] bg-white bg-opacity-40 rounded-[50px] p-[0.8vw] max-lg:p-[1.5vw] max-lg:m-auto max-lg:h-[60%]">
-        <TextBox conversation={filteredMessages} />{" "}
-        <div className=" mt-[-200px] max-lg:mt-[8vw]">
+      <div className="relative self-center top-[5vw] w-[36vw] h-[40vw] max-lg:w-[70%] bg-white bg-opacity-40 rounded-[50px] p-[0.8vw] max-lg:p-[1.5vw] max-lg:m-auto max-lg:h-[60%]">
+        <TextBox conversation={filteredMessages} />
+        <div className="mt-[-200px] max-lg:mt-[8vw]">
           <QuestionBox onSend={sendMessage} />
-        </div>{" "}
+        </div>
       </div>
     </div>
   );
